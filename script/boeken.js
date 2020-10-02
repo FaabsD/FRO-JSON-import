6beb544695073c62aadbd46791cdd78bfa3206b6
Fabian <19959@ma-web.nl>
2020-10-02 14:44:54 +0200

    toont nu boek titels

--- script/boeken.js
+++ script/boeken.js
@@ -1,10 +1,11 @@
-const uitvoer = document.getElementById('boeken');
+const output = document.getElementById('boeken');
 const request = new XMLHttpRequest();
 
 request.onreadystatechange = () => {
     if(request.readyState === 4 && request.status === 200) {
         let result = JSON.parse(request.responseText);
-        console.log(result);
+        books.data = result;
+        books.run();
     } else {
         console.log("readystate: " + request.readyState);
         console.log("status: " + request.status);
@@ -12,3 +13,14 @@ request.onreadystatechange = () => {
 }
 request.open('GET', 'boeken.json', true);
 request.send();
+
+const books = {
+    // Hier wordt een eigenschap data gemaakt
+    run() {
+        let html = "";
+        this.data.forEach(book => {
+            html += `<h3>${book.titel}</h3>`
+        });
+        output.innerHTML = html;
+    }
+}
